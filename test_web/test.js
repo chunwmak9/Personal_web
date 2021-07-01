1661801c754c897df97e6d5b6fcb7481091c8fd3
benny <bennycwmak@gmail.com>
2021-07-02 00:26:48 +0800

    https://github.com/chunwmak9/Personal_web

---
+++ test_web/test.js
@@ -0,0 +1,21 @@
+
+function submitted_comment()
+{
+
+   // var fso = new ActiveXObject("Scripting.FileSystemObject");   
+
+ //   var s   = fso.CreateTextFile("C:\\Users\\benny\\OneDrive\\桌面\\Programming\\Web\\test_web\\action_page.txt", True);
+ 
+
+ 
+
+var comment = document.getElementById("comment").value;
+
+//s.writeline(comment);
+
+ 
+//s.writeline("-----------------------------");
+//s.Close();
+alert(comment);
+
+}
